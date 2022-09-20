387cfefbf2dc4b4c4b8e0f00445634148d35374a
Bildan <bildanjauhary@gmail.com>
2022-09-20 20:44:38 +0700

    updated

--- src/pages/detailProduct.js
+++ src/pages/detailProduct.js
@@ -21,24 +21,6 @@ export default function DetailProduct(){
 
     let { id } = useParams()
 
-    useEffect(() => {
-        //change this to the script source you want to load, for example this is snap.js sandbox env
-        const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
-        //change this according to your client-key
-        const myMidtransClientKey = process.env.REACT_APP_MIDTRANS_CLIENT_KEY;
-
-        let scriptTag = document.createElement("script");
-        scriptTag.src = midtransScriptUrl;
-        // optional if you want to set script attribute
-        // for example snap.js have data-client-key attribute
-        scriptTag.setAttribute("data-client-key", myMidtransClientKey);
-
-        document.body.appendChild(scriptTag);
-        return () => {
-            document.body.removeChild(scriptTag);
-        };
-    }, []);
-
 
     
     let { data: product, refetch } = useQuery('productCache', async () => {
@@ -48,11 +30,6 @@ export default function DetailProduct(){
 
       console.log(product)
 
-    // setProductData({
-    //     price: product.price,
-    //     qty: 1,
-    //     status:"pending",
-    // })
 
     const addToCart = async (e) => {
         try {
